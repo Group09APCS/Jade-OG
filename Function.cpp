5a41bf8112a1b92ec28695b0859ceacb5aa2fbb0
M4rv3l0us <37565007+M4rv3l0us@users.noreply.github.com>
2018-03-20 11:04:09 +0700

    Update Function.cpp

--- Function.cpp
+++ Function.cpp
@@ -49,10 +49,111 @@ void save(const char pathToSaveFile[], Student *head) {
 
 	fout.close();
 }
-bool login(const char username[], const char password[])
+
+void studentMenu()
 {
-	char a[10];
-	cout << "Welcome! This program is used to manage student records!!!" << endl;
-	cout << "First, have you got an account? (y for yes and n for no)" << endl;
-	cin.get(a, 9, '\n');
+	cout << "\n\n  _________________________STUDENT DATABASE_________________________";
+	cout << "\n\n 1: Check - in ";
+	cout << "\n\n 2: View score for a course ";
+	cout << "\n\n 3: Schedule ";
+	cout << "\n\n 4: View your info ";
+	cout << "\n\n 5: Change password ";
+	cout << "\n\n 6: Log out ";
+	cout << "\n\n 7: Exit ";
+
+}
+void acastaffMenu()
+{
+	int choice;
+	cout << "\n\n  _________________________ACADEMIC STAFF DATABASE_________________________";
+	cout << "\n\n 1: Student Management ";
+	cout << "\n\n 2: Classes Management ";
+	cout << "\n\n 3: Courses ";
+	cout << "\n\n 4: Courses Schedule ";
+	cout << "\n\n 5: Search for available course ";
+	cout << "\n\n 6: Scoreboard ";
+	cout << "\n\n 7: Your info ";
+	cout << "\n\n 8: Change password ";
+	cout << "\n\n 9: Log out ";
+	cout << "\n\n 10: Exit ";
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+	{
+		cout << "\n\n 1: Import A Class ";
+		cout << "\n\n 2: Add New Student ";
+		cout << "\n\n 3: Edit Student ";
+		cout << "\n\n 4: Remove Student ";
+		break;
+	}
+	case 2:
+	{
+		cout << "\n\n 1: Import A Class ";
+		cout << "\n\n 2: Create New Class ";
+		cout << "\n\n 3: View List Classes ";
+		cout << "\n\n 4: View List of Student in Class ";
+		break;
+	}
+	case 3:
+	{
+		cout << "\n\n 1: Import A Course ";
+		cout << "\n\n 2: Add New Course ";
+		cout << "\n\n 3: View List Courses ";
+		cout << "\n\n 4: Edit Courses ";
+		cout << "\n\n 5: Remove Courses ";
+		break;
+	}
+	case 4:
+	{
+			cout << "\n\n 1: Import Courses' Schedules ";
+			cout << "\n\n 2: Add New Courses' Schedule ";
+			cout << "\n\n 3: View List Courses' Schedules ";
+			cout << "\n\n 4: Edit Courses' Schedules ";
+			cout << "\n\n 5: Remove Courses' Schedules ";
+			break;
+	}
+	case 5:
+	{
+		cout << "\n\n 1: Search & View Attendance List ";
+		cout << "\n\n 2: Export Attendance List";
+		break;
+	}
+	case 6:
+	{
+		cout << "\n\n 1: Search & View Scoreboard ";
+		cout << "\n\n 2: Export Scoreboard";
+		break;
+	}
+	case 7:
+	{
+		cout << "\n\n Nothing here ";
+		break;
+	}
+	case 8:
+	{
+		cout << "\n\n Nothing here ";
+		break;
+	}
+	case 9:
+	{
+		cout << "\n\n Nothing here ";
+		break;
+	}
+	case 10:
+	{
+		cout << "\n\n Nothing here ";
+		break;
+	}
+	}
+}
+void lecturerMenu()
+{
+	cout << "\n\n  _________________________LECTURER DATABASE_________________________";
+	cout << "\n\n 1: Scoreboard ";
+	cout << "\n\n 2: Edit Grade ";
+	cout << "\n\n 3: Your info ";
+	cout << "\n\n 4: Change password ";
+	cout << "\n\n 5: Log out ";
+	cout << "\n\n 6: Exit ";
 }
