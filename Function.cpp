283058c7974d824bb98e88fe53e3b8fb3cd73eb4
M4rv3l0us <37565007+M4rv3l0us@users.noreply.github.com>
2018-03-21 22:27:28 +0700

    Update Function.cpp

--- Function.cpp
+++ Function.cpp
@@ -64,72 +64,109 @@ void studentMenu()
 }
 void Login()
 {
-	char user[1000],pass[1000];
+	int choice;
+	char name[1000], Username[10], password[1000];
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hConsole, 7);
-	cout<<"	********************************************************************************************"<<endl;
+	cout << "	********************************************************************************************" << endl;
 	SetConsoleTextAttribute(hConsole, 6);
-	cout<<"						FINAL PROJECT"<<endl;
+	cout << "						FINAL PROJECT" << endl;
 	SetConsoleTextAttribute(hConsole, 7);
-	cout<<"	********************************************************************************************"<<endl;
+	cout << "	********************************************************************************************" << endl;
 	SetConsoleTextAttribute(hConsole, 12);
-	cout<<"	          @@HH00                      00                            MM@@HH      @@HH00"<<endl;
-	cout<<"	            @@                        HH                          WW      HH  WW"<<endl;
-	cout<<"	            MM      WWOOMM            @@    WWOOMM                00      @@  00"<<endl;
-	cout<<"	            OO            MM    00WWOOMM  HH      MM              HH      MM  HH        "<<endl;
-	cout<<"	            WW      HH00WWOO  @@      OO  @@HH00WWOO    HH00WW    @@      OO  @@    WWOO"<<endl;
-	cout<<"	    OO      00    MM      WW  MM      WW  MM                      MM      WW  MM      WW"<<endl;
-	cout<<"	      OOMM@@        MM@@HH00    MM@@HH00    MM@@HH                  MM@@HH      MM@@HH"<<endl;
+	cout << "	          @@HH00                      00                            MM@@HH      @@HH00" << endl;
+	cout << "	            @@                        HH                          WW      HH  WW" << endl;
+	cout << "	            MM      WWOOMM            @@    WWOOMM                00      @@  00" << endl;
+	cout << "	            OO            MM    00WWOOMM  HH      MM              HH      MM  HH        " << endl;
+	cout << "	            WW      HH00WWOO  @@      OO  @@HH00WWOO    HH00WW    @@      OO  @@    WWOO" << endl;
+	cout << "	    OO      00    MM      WW  MM      WW  MM                      MM      WW  MM      WW" << endl;
+	cout << "	      OOMM@@        MM@@HH00    MM@@HH00    MM@@HH                  MM@@HH      MM@@HH" << endl;
 	SetConsoleTextAttribute(hConsole, 7);
-	cout<<"	********************************************************************************************"<<endl;
+	cout << "	********************************************************************************************" << endl;
 	SetConsoleTextAttribute(hConsole, 6);
-	cout<<"					STUDENT MANAGEMENT SYSTEM"<<endl;
+	cout << "					STUDENT MANAGEMENT SYSTEM" << endl;
 	SetConsoleTextAttribute(hConsole, 7);
-	cout<<"	********************************************************************************************"<<endl;
-	cout<<"						<<LOGIN>>"<<endl;
-	cout<<"					Enter UserName: "<<endl;//khi co code cua Giang thi xoa cai endl
-	//cin>>user;
-	cout<<"					Enter Password: "<<endl;
+	cout << "	********************************************************************************************" << endl;
+	cout << "						<<LOGIN>>" << endl;
+	cout << "					Enter UserName: " << endl;//khi co code cua Giang thi xoa cai endl
+														  //cin>>user;
+	cout << "					Enter Password: " << endl;
 	//cin>>pass;
 	//them code kiem tra vao,sai bat nhap lai
+	cout << "                   New to Jade-OG? Get an account here (3)";
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+	{
+		cout << "Nothing here \n";
+	}
+	case 2:
+	{
+		cout << "Nothing here \n";
+	}
+	case 3:
+	{
+		cout << "Please input your fullname: ";
+		cin.ignore(1000, '\n');
+		cin.get(name, 1000, '\n');
+		User* cur = new User;
+		cur->fullName = *name;
+		generateid(name, Username);
+		cur->userName = *Username;
+		int a = pass();
+		int x = log(a) / log(10) + 1;
+		for (int i = x; i > 0; i--)
+		{
+			cur->password[i] = a / pow(10, i - 1) + 48;
+		}
+		cout << "Your username: " << cur->userName << endl;
+		cout << "Your password: " << cur->password << endl;
+			cout << "Please set a new password: ";
+			cin.ignore(1000, '\n');
+			cin.get(cur->password, 1000, '\n');
+			
+			cout << "Your username: " << cur->userName << endl;
+			cout << "Your password: " << cur->password << endl;
+	}
+	}
 	system("pause");
 	system("cls");
-	cout<<"______________________________________________________WELCOME___________________________________________________________"<<endl;
-	int choice;
+	cout << "______________________________________________________WELCOME___________________________________________________________" << endl;
+
 	do
 	{
 		cout << endl
-		<< " 1 - Show the menu.\n"
-		<< " 2 - View info.\n"
-		<< " 4 - Change password.\n"
-		<< " 5 - Exit.\n"
-		<< " What do you want to do: ";
+			<< " 1 - Show the menu.\n"
+			<< " 2 - View info.\n"
+			<< " 4 - Change password.\n"
+			<< " 5 - Exit.\n"
+			<< " What do you want to do: ";
 		cin >> choice;
 		switch (choice)
 		{
-			case 1:
+		case 1:
 			//code 
 			break;
-			case 2:
+		case 2:
 			//code 
 			break;
-			case 3:
+		case 3:
 			//code
 			break;
-			case 4:
+		case 4:
 			//code
 			break;
-			case 5:
+		case 5:
 			cout << "End of Program.\n";
 			break;
-			default:
+		default:
 			cout << "Not a Valid Choice. \n"
-			<< "Choose again.\n";
+				<< "Choose again.\n";
 			break;
-		}	
+		}
 
-	}
-	while (choice <1&&choice>5);
+	} while (choice <1 && choice>5);
 }
 
 void acastaffMenu()
@@ -176,12 +213,12 @@ void acastaffMenu()
 	}
 	case 4:
 	{
-			cout << "\n\n 1: Import Courses' Schedules ";
-			cout << "\n\n 2: Add New Courses' Schedule ";
-			cout << "\n\n 3: View List Courses' Schedules ";
-			cout << "\n\n 4: Edit Courses' Schedules ";
-			cout << "\n\n 5: Remove Courses' Schedules ";
-			break;
+		cout << "\n\n 1: Import Courses' Schedules ";
+		cout << "\n\n 2: Add New Courses' Schedule ";
+		cout << "\n\n 3: View List Courses' Schedules ";
+		cout << "\n\n 4: Edit Courses' Schedules ";
+		cout << "\n\n 5: Remove Courses' Schedules ";
+		break;
 	}
 	case 5:
 	{
@@ -226,4 +263,40 @@ void lecturerMenu()
 	cout << "\n\n 4: Change password ";
 	cout << "\n\n 5: Log out ";
 	cout << "\n\n 6: Exit ";
+}
+
+void generateid(char name[1000], char Username[])
+{
+	int i1 = 1, a=0 ;
+
+	Username[0] = tolower(name[0]);
+	for (int i = 0; i < strlen(name); ++i)
+	{
+		if (name[i] == ' ')
+			a = i;
+	}
+
+	for (int i = 0; i < (strlen(name)); ++i)
+	{
+		if (name[i] == ' ')
+		{
+			Username[i1] = tolower(name[i + 1]);
+			++i1;
+		}
+	}
+
+	i1--;
+
+	while (a < strlen(name))
+	{
+		Username[i1] = tolower(name[a + 1]);
+		i1++; a++;
+	}
+}
+
+int pass()
+{
+	srand(time(NULL));
+	int a = rand();
+	return a;
 }
